db71e43a143b62815f897a195f5e8716efcb3c4c
고유림 <qpeijty1@naver.com>
2021-02-23 23:30:25 +0900

    브루트포스 - N과 M 15649번 - N과 M (1)

--- src/beakjoon/Main4.java
+++ src/beakjoon/Main4.java
@@ -4,12 +4,32 @@ import java.io.*;
 import java.util.*;
 
 public class Main4 {
-
-	public static void main(String[] args)  throws IOException {
-		// TODO Auto-generated method stub
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
+	public static boolean[] c = new boolean[10];
+	public static int[] a = new int[10];
+	public static void main(String[] args) {
+		Main4 m = new Main4();
+        Scanner sc = new Scanner(System.in);
+        int range = sc.nextInt();
+        int count = sc.nextInt();
+       
+        m.go(0, range, count);
+	}
+	public void go(int index, int n, int m) {
+		if(index == m) {
+			for(int i=0; i<m; i++ ) {
+				System.out.print(a[i]);
+				if(i != m-1) System.out.print(" ");
+			}
+			System.out.print("\n");
+			return;
+		}
+		for(int i = 1; i<=n; i++) {
+			if(c[i]) continue;
+			c[i] = true; 
+			a[index] = i;
+			go(index+1, n ,m);
+			c[i] = false;
+		}
 	}
 
 }
