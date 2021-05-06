9bea20cf8a8a5db3a5200ead77afed1c721bb404
고유림 <qpeijty1@naver.com>
2021-05-06 23:11:12 +0900

    /add/ sorting part : 접미사 배열

---
+++ src/beakjoon/Q11656.java
@@ -0,0 +1,20 @@
+package beakjoon;
+
+import java.util.*;
+public class Q11656 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+		StringBuilder sb = new StringBuilder(str);
+		String[] arr = new String[str.length()];
+		for(int i = 0; i<str.length(); i++) {
+			arr[i] = sb.substring(i);
+		}
+		Arrays.sort(arr);
+		for(String s : arr) {
+			System.out.println(s);
+		}
+	}
+
+}
