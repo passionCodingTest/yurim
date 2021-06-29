42370a2cb8307bd372ce04d402ff5b3da2b526cb
고유림 <qpeijty1@naver.com>
2021-06-29 16:58:00 +0900

    /add/ 210621

--- src/beakjoon/Q10835.java
+++ src/beakjoon/Q10835.java
@@ -1,41 +1,41 @@
 package beakjoon;
+import java.io.*;
 import java.util.*;
 public class Q10835 {
 	static int ans = 0, k;
 	static int[] left, right;
-	public static void main (String[] args){
-		Scanner sc = new Scanner(System.in);
-		k = sc.nextInt();
+	static int[][] dp;
+	public static void main (String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		k = Integer.parseInt(br.readLine());
 		left = new int[k]; right = new int[k];
-		for(int i = 0; i<k; i++) left[i] = sc.nextInt();
-		for(int i = 0; i<k; i++) right[i] = sc.nextInt();
-		//조건 : 언제든지 left 혹은 left&right를 버릴 수 있다. *이때 점수는 없다*
-		//조건 : left > right : right만 담기 *점수 획득*
-//		go(0,0,0,0);
-		
-		//왼쪽 : 오른
-		int[][] dp= new int[k][k];
-		int l=0,r=0;
-		while(l<k&&r<k) {
+		dp = new int[k][k];
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		for(int i = 0; i<k; i++) left[i] = Integer.parseInt(st.nextToken());
+		st = new StringTokenizer(br.readLine()," ");
+		for(int i = 0; i<k; i++) right[i] = Integer.parseInt(st.nextToken());
+		for(int[] arr : dp){
+			Arrays.fill(arr,-1);
 		}
-		System.out.println(ans);
+		ans = go(0,0);
+		bw.write(ans+"");
+		bw.flush();
+		bw.close();
+		br.close();
 	}
-//	static public void go(int l, int r, int buket, int cnt){
-//		if(l==k || r==k) {
-//			ans = Math.max(ans, buket);
-//			return;
-//		}
-//		int a = left[l];
-//		int b = right[r];
-//		if(a > b) {
-//			//오른쪽 카드 버리기
-//			go(l,r+1,buket+b,cnt+1);
-//		}
-//		else{
-//			//왼쪽만 버리기
-//			go(l+1, r, buket, cnt+1);
-//			//왼쪽 & 오른쪽 버리기
-//			go(l+1, r+1, buket , cnt+1);
-//		}
-//	}
+	static public int go(int l, int r){
+		if(l==k || r==k) return 0;
+		if(dp[l][r] != -1) return dp[l][r];
+		dp[l][r] = 0;
+		
+		int resultWithOutLeft = go(l+1, r);
+		int resultWithOutLeftAndRight = go(l+1, r+1);
+		
+		int result = 0;
+		if(left[l] > right[r]){
+			result = right[r] + go(l, r+1);
+		}
+		return dp[l][r] = Math.max(resultWithOutLeft, Math.max(resultWithOutLeftAndRight, result));
+	}
 }
